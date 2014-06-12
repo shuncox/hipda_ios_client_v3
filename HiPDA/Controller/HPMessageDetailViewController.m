7c54ec60c23f3dabfa84f79669d6c6ca3c74aef8
wujichao <wujichao@gmail.com>
2014-06-12 19:19:56 +0800

    加入短消息范围以及修复crash bug

--- HiPDA/Controller/HPMessageDetailViewController.m
+++ HiPDA/Controller/HPMessageDetailViewController.m
@@ -15,10 +15,19 @@
 #import <UIImageView+WebCache.h>
 #import <SVProgressHUD.h>
 #import "NSUserDefaults+Convenience.h"
+#import "UIAlertView+Blocks.h"
+#import "UIBarButtonItem+ImageItem.h"
 
-@interface HPMessageDetailViewController () <JSMessagesViewDataSource, JSMessagesViewDelegate>
+typedef enum {
+    HPMessageRangeLatest = 3,
+    HPMessageRangeCurrentWeek,
+    HPMessageRangeAll
+} HPMessageRange;
+
+@interface HPMessageDetailViewController () <JSMessagesViewDataSource, JSMessagesViewDelegate, UIActionSheetDelegate>
 
 @property (strong, nonatomic) NSMutableArray *messages;
+@property (nonatomic, assign) HPMessageRange range;
 
 @end
 
@@ -32,6 +41,8 @@
     self.dataSource = self;
     [super viewDidLoad];
     
+    _range = HPMessageRangeLatest;
+    
     self.title = [NSString stringWithFormat:@"与 %@", _user.username];
     [self setBackgroundColor:[UIColor whiteColor]];
     
@@ -41,7 +52,7 @@
     self.messageInputView.textView.placeHolder = NSLocalizedString(@"New Message", nil);
     self.sender = [NSStandardUserDefaults stringForKey:kHPAccountUserName];
     
-    
+    /*
     // refresh btn
     UIBarButtonItem *refreshButtonItem = [
                                           [UIBarButtonItem alloc] initWithTitle:@"刷新"
@@ -49,6 +60,12 @@
                                           target:self
                                           action:@selector(refresh:)];
     self.navigationItem.rightBarButtonItem = refreshButtonItem;
+    */
+    UIBarButtonItem *moreBI = [UIBarButtonItem barItemWithImage:[UIImage imageNamed:@"more.png"]
+                                                           size:CGSizeMake(40.f, 40.f)
+                                                         target:self
+                                                         action:@selector(action:)];
+    self.navigationItem.rightBarButtonItem = moreBI;
     
     // gesture
     UISwipeGestureRecognizer *rightSwipeGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(back:)];
@@ -62,6 +79,10 @@
     [self refresh:nil];
 }
 
+- (void)dealloc {
+    NSLog(@"dealloc");
+}
+
 #pragma mark -
 
 - (void)back:(id)sender {
@@ -71,11 +92,10 @@
 
 - (void)refresh:(id)sender {
     
-    
-    
+    __typeof__(self) __weak weakSelf = self;
     [SVProgressHUD showWithStatus:@"载入中..."];
-    [HPMessage loadMessageDetailWithUid:_user.uid
-                              daterange:5
+    [HPMessage loadMessageDetailWithUid:weakSelf.user.uid
+                              daterange:weakSelf.range
                                   block:^(NSArray *lists, NSError *error)
      {
          if (error) {
@@ -85,18 +105,79 @@
          } else if ([lists count]){
              [SVProgressHUD dismiss];
              
-             _messages = [NSMutableArray arrayWithArray:lists];
-             [self.tableView reloadData];
+             weakSelf.messages = [NSMutableArray arrayWithArray:lists];
+             [weakSelf.tableView reloadData];
              
-             // try to avoid crash
-             dispatch_async(dispatch_get_main_queue(), ^{
-                 [self scrollToBottomAnimated:YES];
-             });
+             // animate = yes = crash often
+             [weakSelf scrollToBottomAnimated:NO];
              
          } else {
-             [SVProgressHUD showErrorWithStatus:@"没有记录"];
+             [SVProgressHUD dismiss];
+             
+             NSString *tip = nil;
+             switch (weakSelf.range) {
+                 case HPMessageRangeLatest:
+                     tip = @"最近三天没有新短消息\n是否查看更早时间的短消息历史？";
+                     break;
+                 case HPMessageRangeCurrentWeek:
+                     tip = @"本周没有新短消息\n是否查看更早时间的短消息历史？";
+                     break;
+                 case HPMessageRangeAll:
+                     tip = @"未找到任何记录\n是否重试？";
+                     break;
+                 default:
+                     break;
+             }
+             
+             [UIAlertView showConfirmationDialogWithTitle:@"提示"
+                                                  message:tip
+                                                  handler:^(UIAlertView *alertView, NSInteger buttonIndex)
+              {
+                  if (buttonIndex != [alertView cancelButtonIndex]) {
+                      
+                      if (weakSelf.range < HPMessageRangeAll) {
+                          weakSelf.range++;
+                      }
+                      
+                      [weakSelf refresh:nil];
+                  }
+              }];
          }
      }];
+}
+
+- (void)test {
+    [self scrollToBottomAnimated:YES];
+}
+
+#pragma mark - actions
+- (void)action:(id)sender {
+    
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:nil otherButtonTitles:@"刷新", @"最近三天", @"本周", @"全部", nil];
+    
+    [actionSheet showInView:self.view];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    switch (buttonIndex) {
+        case 0:
+            [self refresh:nil];
+            break;
+        case 1:
+            _range = HPMessageRangeLatest;
+            [self refresh:nil];
+            break;
+        case 2:
+            _range = HPMessageRangeCurrentWeek;
+            [self refresh:nil];
+            break;
+        case 3:
+            _range = HPMessageRangeAll;
+            [self refresh:nil];
+            break;
+        default:
+            break;
+    }
 }
 
 
