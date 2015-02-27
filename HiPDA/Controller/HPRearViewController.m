fe6cc236d7f47a70bdf31f56cabfefe47f79bb76
Jichao Wu <wujichao@gmail.com>
2015-02-27 22:23:43 +0800

    rowHeight for 4s

--- HiPDA/Controller/HPRearViewController.m
+++ HiPDA/Controller/HPRearViewController.m
@@ -273,17 +273,19 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    CGFloat rowHeight = (kScreenHeight == 480.f ? 40.f : 44.f);
+    
     if (indexPath.row == 0) {
         return TOP_CELL_HEIGHT;
     } else if (indexPath.row == _vc_classes.count){
         
         CGFloat screent_height = self.view.bounds.size.height;
-        CGFloat other_hight_sum = TOP_CELL_HEIGHT + (_vc_classes.count-1)*44.f + _fids.count*44.f;
+        CGFloat other_hight_sum = TOP_CELL_HEIGHT + (_vc_classes.count-1)*rowHeight + _fids.count*rowHeight;
         
         return MAX(screent_height - other_hight_sum, 0.f);
         
     } else {
-        return 44.f;
+        return rowHeight;
     }
 }
 
