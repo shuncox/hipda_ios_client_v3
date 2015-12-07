4ad89b26800a295ea5015d4b58ff09f5bcc716ec
Jichao Wu <wujichao@gmail.com>
2015-12-07 19:35:42 +0800

    在线参数区分App版本

--- HiPDA/Helper/UMOnlineConfig+BOOL.m
+++ HiPDA/Helper/UMOnlineConfig+BOOL.m
@@ -8,10 +8,13 @@
 
 #import "UMOnlineConfig+BOOL.h"
 
+#define VERSION ([[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"])
+
 @implementation UMOnlineConfig (BOOL)
 
 + (BOOL)getBoolConfigWithKey:(NSString *)key defaultYES:(BOOL)defaultYES
 {
+    key = [NSString stringWithFormat:@"%@-%@", key, VERSION];
     NSString *s = [self.class getConfigParams:key] ?: (defaultYES ? @"1" : @"0");
     return [s integerValue] == 1;
 }
