94e8cd6507239cc31c7bcc57490bc25964fbb274
Jichao Wu <wujichao@gmail.com>
2016-01-21 19:57:45 +0800

    reporter参数encode

--- leancloud/hpimg/Reporter.js
+++ leancloud/hpimg/Reporter.js
@@ -1,8 +1,11 @@
 var AV = require('leanengine');
+var Util = require('./Util');
+
 var PUSH_URL = 'http://sc.'+'ft'+'qq'+'.com/SCU898Tec0e33d62f0fc6d2da4813732f4726ca569da35b97d4c.send';
 
 exports.report = function(text, desp) {
-	var url = PUSH_URL + '?text='+text+'&desp='+desp;
+	var p = Util.serialize({text:text, desp:desp});
+	var url = PUSH_URL + '?text='+p;
 	AV.Cloud.httpRequest({
 		url: url,
 		success: function(httpResponse) {
