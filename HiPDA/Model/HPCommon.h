9f2beaafbffb7c832aeb15f76309ccba08cd7c43
Jichao Wu <wujichao@gmail.com>
2016-09-15 19:19:16 +0800

    应alu要求, img域名在线可配置

--- HiPDA/Model/HPCommon.h
+++ HiPDA/Model/HPCommon.h
@@ -128,7 +128,7 @@
 
 #define HP_WWW_BASE_URL @"www.hi-pda.com"
 #define HP_CNC_BASE_URL @"cnc.hi-pda.com"
-#define HP_IMG_BASE_URL @"img.hi-pda.com"
+#define HP_IMG_BASE_URL ([UMOnlineConfig getConfigParams:@"image_domain"] ?: @"img.hi-pda.com")
 #define HP_CDN_BASE_URL @"7xq2vp.com1.z0.glb.clouddn.com"
 #define HP_CDN_URL_SUFFIX (@"-w600")
 extern NSString *HP_WWW_BASE_IP;
