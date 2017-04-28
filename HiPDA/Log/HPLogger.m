d07e364db339900fbfb40a2298477fec8c9e2377
Jichao Wu <wujichao@gmail.com>
2017-04-28 10:06:19 +0800

    release里,只开file logger

--- HiPDA/Log/HPLogger.m
+++ HiPDA/Log/HPLogger.m
@@ -15,9 +15,10 @@
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
+#ifdef DEBUG
         [DDLog addLogger:[DDTTYLogger sharedInstance]]; // TTY = Xcode console
         [DDLog addLogger:[DDASLLogger sharedInstance]]; // ASL = Apple System Logs
-        
+#endif
         DDFileLogger *fileLogger = [[DDFileLogger alloc] init]; // File Logger
         fileLogger.rollingFrequency = 60 * 60 * 24; // 24 hour rolling
         fileLogger.logFileManager.maximumNumberOfLogFiles = 7;
