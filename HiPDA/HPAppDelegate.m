a6856fa9a895146a871cbf55276f9f279340c55c
Jichao Wu <wujichao@gmail.com>
2017-05-14 11:08:12 +0800

    log

--- HiPDA/HPAppDelegate.m
+++ HiPDA/HPAppDelegate.m
@@ -222,6 +222,7 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    DDLogInfo(@"[APP] applicationWillTerminate");
 }
 
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
