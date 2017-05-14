7df43ca3834b62159bb4ba0177a9555f556ecb1b
Jichao Wu <wujichao@gmail.com>
2017-05-14 13:44:24 +0800

    log

--- HiPDA/Helper/UIViewController+Tracking.m
+++ HiPDA/Helper/UIViewController+Tracking.m
@@ -13,46 +13,76 @@
 //http://nshipster.com/method-swizzling/
 @implementation UIViewController (Tracking)
 
+static inline void SwizzleSelector(Class class, SEL originalSelector, SEL swizzledSelector)
+{
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    
+    // When swizzling a class method, use the following:
+    // Class class = object_getClass((id)self);
+    // ...
+    // Method originalMethod = class_getClassMethod(class, originalSelector);
+    // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
+    
+    BOOL didAddMethod =
+    class_addMethod(class,
+                    originalSelector,
+                    method_getImplementation(swizzledMethod),
+                    method_getTypeEncoding(swizzledMethod));
+    
+    if (didAddMethod) {
+        class_replaceMethod(class,
+                            swizzledSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         Class class = [self class];
         
-        SEL originalSelector = @selector(viewWillAppear:);
-        SEL swizzledSelector = @selector(xxx_viewWillAppear:);
-        
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        
-        // When swizzling a class method, use the following:
-        // Class class = object_getClass((id)self);
-        // ...
-        // Method originalMethod = class_getClassMethod(class, originalSelector);
-        // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
-        
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod));
-        
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+        SwizzleSelector(class, @selector(viewDidLoad), @selector(xxx_viewDidLoad));
+        SwizzleSelector(class, @selector(viewWillAppear:), @selector(xxx_viewWillAppear:));
+        SwizzleSelector(class, @selector(viewDidAppear:), @selector(xxx_viewDidAppear:));
+        SwizzleSelector(class, @selector(viewWillDisappear:), @selector(xxx_viewWillDisappear:));
+        SwizzleSelector(class, @selector(viewDidDisappear:), @selector(xxx_viewDidDisappear:));
     });
 }
 
 #pragma mark - Method Swizzling
 
+- (void)xxx_viewDidLoad
+{
+    [self xxx_viewDidLoad];
+    DDLogInfo(@"[PAGE][DidLoad][%@]", NSStringFromClass(self.class));
+}
+
 - (void)xxx_viewWillAppear:(BOOL)animated {
     [self xxx_viewWillAppear:animated];
     HPCrashLog(@"-> %@", NSStringFromClass(self.class));
-    DDLogInfo(@"[PAGE][%@]", NSStringFromClass(self.class));
+    DDLogInfo(@"[PAGE][WillAppear][%@]", NSStringFromClass(self.class));
+}
+
+- (void)xxx_viewDidAppear:(BOOL)animated
+{
+    [self xxx_viewDidAppear:animated];
+    DDLogInfo(@"[PAGE][DidAppear][%@]", NSStringFromClass(self.class));
+}
+
+- (void)xxx_viewWillDisappear:(BOOL)animated
+{
+    [self xxx_viewWillDisappear:animated];
+    DDLogInfo(@"[PAGE][WillDisappear][%@]", NSStringFromClass(self.class));
+}
+
+- (void)xxx_viewDidDisappear:(BOOL)animated
+{
+    [self xxx_viewDidDisappear:animated];
+    DDLogInfo(@"[PAGE][DidDisappear][%@]", NSStringFromClass(self.class));
 }
 
 @end
