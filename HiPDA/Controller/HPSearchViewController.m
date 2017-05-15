72a4857317225e9da75631efa794a102a96a9c21
Jichao Wu <wujichao@gmail.com>
2017-05-15 19:31:08 +0800

    ⚡️优化搜索排序

--- HiPDA/Controller/HPSearchViewController.m
+++ HiPDA/Controller/HPSearchViewController.m
@@ -500,9 +500,22 @@
             
             NSMutableArray *results = [NSMutableArray array];
             [[HPDatabase sharedDb].queue inDatabase:^(FMDatabase *db) {
-                FMResultSet *resultSet = [db executeQuery:@"SELECT * FROM user WHERE username LIKE ?", [NSString stringWithFormat:@"%%%@%%", key]];
+                
+                // 优化排序 http://stackoverflow.com/questions/10070508/sqlite-like-order-by-match-query
+                NSString *sql = [NSString stringWithFormat:
+                               @"SELECT *\n"
+                                "FROM user\n"
+                                "WHERE username LIKE '%%%@%%'\n"
+                                "ORDER BY (\n"
+                                    "CASE WHEN username = '%@' THEN 1\n"
+                                    "WHEN username LIKE '%@%%' THEN 2\n"
+                                    "ELSE 3 END\n"
+                                "), username\n"
+                                "LIMIT 1000\n", key, key, key];
+                
+                FMResultSet *resultSet = [db executeQuery:sql];
+                
                 while ([resultSet next]) {
-                    
                     NSString *username = [resultSet stringForColumnIndex:0];
                     NSString *uid = [resultSet stringForColumnIndex:1];
                     
